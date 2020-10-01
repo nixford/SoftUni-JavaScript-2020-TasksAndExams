1591dbdd210947e151099c34ad9e3afcbaa0cec8
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-10-01 11:35:37 +0300

    Adding solutions of DOM-Lab - Growing Word and Visited Websites

--- DOM-Lab/03. Growing Word/solution.js
+++ DOM-Lab/03. Growing Word/solution.js
@@ -1,4 +1,11 @@
-function growingWord() {
+function growingWord() {	
+  const colors = ['blue', 'green', 'red'];
 
-  //TODO...
+    const paragraph = document.getElementsByTagName('p')[2];
+
+    let color = paragraph.style.color;
+    paragraph.style.color = !color ? 'blue' : colors[(colors.indexOf(color) + 1) % colors.length];
+
+    let fontSize = paragraph.style.fontSize.replace('px', '');
+    paragraph.style.fontSize = (!fontSize ? '2' : fontSize * 2) + 'px';
 }
