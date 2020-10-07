9da8727a88a89f4552d18540b7cbefbbbc5848e1
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-10-07 19:03:03 +0300

    removing folder

--- DOM-Manipulations/01. List-Of-Items/app.js
+++
@@ -1,8 +0,0 @@
-function addItem() {    
-    let parentElement = document.getElementById('items');
-    let inputElement = document.getElementById('newItemText');
-    let liElement = document.createElement('li');
-    liElement.innerHTML = inputElement.value;
-    inputElement.value = '';
-    parentElement.appendChild(liElement);
-}
