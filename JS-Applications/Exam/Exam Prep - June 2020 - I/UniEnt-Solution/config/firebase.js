aabb1d3b98c4fe918ae0b5012ce3186f565c55b6
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-11-14 22:30:16 +0200

    Adding partial solution of Exam Prep - June 2020 - I

---
+++ JS-Applications/Exam/Exam Prep - June 2020 - I/UniEnt-Solution/config/firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB6v-A-6UdpsC0-fV1pgJiXxeAYLOwjGCo",
+    authDomain: "nixfordproject.firebaseapp.com",
+    databaseURL: "https://nixfordproject.firebaseio.com",
+    projectId: "nixfordproject",
+    storageBucket: "nixfordproject.appspot.com",
+    messagingSenderId: "770098511644",
+    appId: "1:770098511644:web:0befd2713a0a3e2678c4d9",
+    measurementId: "G-LZGFGVYMB8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
