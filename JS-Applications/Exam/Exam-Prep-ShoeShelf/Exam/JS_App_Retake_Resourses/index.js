521d00ebad083b5f9652b961dc78d12cf5cd9d28
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-12-04 14:24:47 +0200

    Adding final solution

--- JS-Applications/Exam/Exam-Prep-ShoeShelf/Exam/JS_App_Retake_Resourses/index.js
+++ JS-Applications/Exam/Exam-Prep-ShoeShelf/Exam/JS_App_Retake_Resourses/index.js
@@ -1,35 +1,36 @@
 const UserModel = firebase.auth();
 const DB = firebase.firestore();
 
-const app = Sammy('#root', function(){
+const app = Sammy('#root', function () {
 
     this.use('Handlebars', 'hbs');
 
     // Home routes
-    this.get('#/home', function(context){
+    this.get('#/home', function (context) {
         DB.collection('offers')
-        .get()
-        .then((response) => {
-            context.offers = [];
-            response.forEach(offer => {
-                context.offers.push({ id: offer.id, ...offer.data() });
-            });
-            extendContext(context)
-            .then(function() {
-                this.partial('./templates/home.hbs');
-            }) 
-        })        
+            .get()
+            .then((response) => {
+                // context.offers = [];
+                context.offers = response.docs.map((offer) => { return { id: offer.id, ...offer.data() } });
+                // response.forEach(offer => {
+                //     context.offers.push({ id: offer.id, ...offer.data() });
+                // });
+                extendContext(context)
+                    .then(function () {
+                        this.partial('./templates/home.hbs');
+                    })
+            })
     });
 
     // User routes
-    this.get('#/register', function(context){
+    this.get('#/register', function (context) {
         extendContext(context)
-            .then(function() {
+            .then(function () {
                 this.partial('./templates/register.hbs');
-            })         
+            })
     });
 
-    this.post('#/register', function(context){
+    this.post('#/register', function (context) {
         const { email, password, rePassword } = context.params;
 
         if (password !== rePassword) {
@@ -40,29 +41,29 @@ const app = Sammy('#root', function(){
             .then((userData) => {
                 // saveUserData(userData);
                 this.redirect('#/login');
-             })
+            })
             .catch(errorHandler);
     });
 
-    this.get('#/login', function(context){
+    this.get('#/login', function (context) {
         extendContext(context)
-            .then(function() {
+            .then(function () {
                 this.partial('./templates/login.hbs');
-            })    
+            })
     });
 
-    this.post('#/login', function(context){
+    this.post('#/login', function (context) {
         const { email, password } = context.params;
 
         UserModel.signInWithEmailAndPassword(email, password)
             .then((userData) => {
                 saveUserData(userData);
                 this.redirect('#/home');
-             })
+            })
             .catch(errorHandler);
     });
-    
-    this.get('#/logout', function(context){
+
+    this.get('#/logout', function (context) {
         UserModel.signOut()
             .then(() => {
                 clearUserData();
@@ -72,14 +73,14 @@ const app = Sammy('#root', function(){
     });
 
     // Offers routes
-    this.get('#/create-offer', function(context){
+    this.get('#/create-offer', function (context) {
         extendContext(context)
-            .then(function() {
+            .then(function () {
                 this.partial('./templates/createOffer.hbs');
-            })        
+            })
     });
 
-    this.post('#/create-offer', function(context){
+    this.post('#/create-offer', function (context) {
         const { productName, price, imageUrl, description, brand } = context.params;
 
         DB.collection('offers').add({
@@ -98,31 +99,112 @@ const app = Sammy('#root', function(){
             .catch(errorHandler);
     });
 
-    this.get('#/edit-offer/:id', function(context){        
+    this.get('#/edit-offer/:id', function (context) {
         extendContext(context)
-            .then(function() {
+            .then(function () {
                 this.partial('./templates/editOffer.hbs');
-            })    
+            })
     });
 
-    this.get('#/details/:offerId', function(context){  
+    this.get('#/details/:offerId', function (context) {
+        const { offerId } = context.params;
+        DB.collection('offers')
+            .doc(offerId)
+            .get()
+            .then((response) => {
+
+                const { uid } = getUserData();
+
+                const actualOfferData = response.data();
+                const imTheSalesMan = actualOfferData.salesman === uid;
+
+                const userIndex = actualOfferData.clients.indexOf(uid);
+                const imInTheClientsList = userIndex > -1;
+
+                context.offer = { ...actualOfferData, imTheSalesMan, id: offerId, imInTheClientsList };
+
+                extendContext(context)
+                    .then(function () {
+                        this.partial('./templates/details.hbs');
+                    })
+            })
+
+    });
+
+    this.get('#/delete/:offerId', function (context) {
+
         const { offerId } = context.params;
 
         DB.collection('offers')
-        .doc(offerId)
-        .get()
-            .then((response) => {
-                const actualOfferData = response.data();
-                const imTheSalesMan = actualOfferData.salesman === getUserData().uid;
-
-                context.offer = {...response.data(), imTheSalesMan};
-                extendContext(context)
-                .then(function() {
-                    this.partial('./templates/details.hbs');
-                })    
-            })        
-        
+            .doc(offerId)
+            .delete()
+            .then(() => {
+                this.redirect('#/home');
+            })
+            .catch(errorHandler);
     });
+
+    this.get('#/edit/:offerId', function (context) {
+        const { offerId } = context.params;
+
+        DB.collection('offers')
+            .doc(offerId)
+            .get()
+            .then((response) => {
+                context.offer = { id: offerId, ...response.data() };
+
+                extendContext(context)
+                    .then(function () {
+                        this.partial('./templates/editOffer.hbs');
+                    })
+            })
+    });
+    // Update function for Firebase
+    this.post('#/edit/:offerId', function (context) {
+        const { offerId, productName, price, brand, description, imageUrl } = context.params;
+
+        DB.collection('offers')
+            .doc(offerId)
+            .get()
+            .then((response) => {
+                return DB.collection('offers')
+                    .doc(offerId)
+                    .set({
+                        ...response.data(),
+                        productName,
+                        price,
+                        brand,
+                        description,
+                        imageUrl
+                    })
+            })
+            .then((response) => {
+                this.redirect(`#/details/${offerId}`)
+            })
+            .catch(errorHandler)
+    })
+
+    this.get('#/buy/:offerId', function (context) {
+
+        const { offerId } = context.params;
+        const { uid } = getUserData();
+
+        DB.collection('offers')
+            .doc(offerId)
+            .get()
+            .then((response) => {
+                const offerData = { ...response.data() };
+                offerData.clients.push(uid);
+                return DB.collection('offers')
+                    .doc(offerId)
+                    .set(offerData)
+            })
+            .then(() => {
+                this.redirect(`#/details/${offerId}`)
+            })
+            .catch(errorHandler);
+    });
+
 });
 
 (() => {
@@ -148,9 +230,9 @@ function errorHandler(error) {
 }
 
 // Saving data to local storage if user isLoggedIn
-function saveUserData(data){
+function saveUserData(data) {
     const { user: { email, uid } } = data;
-    localStorage.setItem('user', JSON.stringify({ email, uid } ));
+    localStorage.setItem('user', JSON.stringify({ email, uid }));
 }
 
 function getUserData() {
