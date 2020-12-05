146128e9607ff82deb36aeaf5792e84194e01346
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-12-05 15:57:49 +0200

    Adding full solution

--- JS-Applications/Exam/Exam Prep - June 2020 - I/UniEnt-Solution/controller/auth.js
+++
@@ -1,8 +0,0 @@
-export function saveUserInfo(userInfo){
-    sessionStorage.setItem('user', userInfo);
-}
-
-export function setHeader(ctx){
-    ctx.isAuth = sessionStorage.getItem('user') !== null;
-    ctx.user = sessionStorage.getItem('user');
-}
--- JS-Applications/Exam/Exam Prep - June 2020 - I/UniEnt-Solution/controller/partials.js
+++
@@ -1,4 +0,0 @@
-export default {
-    header: './view/common/header.hbs',
-    footer: './view/common/footer.hbs'
-}
