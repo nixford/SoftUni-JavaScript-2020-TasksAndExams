64743dde17131c8e412fb29b90e2a73d47a23deb
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-04-06 19:31:49 +0300

    Add functions syntaxes

---
+++ JS-Advanced/Tasks/Advanced-Functions-Lab/Lab-Coding.js
@@ -0,0 +1,24 @@
+function add(a, b) { 
+	return a + b;
+}
+
+// It is same as:
+// let add = function(a, b) { 
+// 	return a + b;
+// }
+
+// let add = (a, b) => { 
+// 	return a + b;
+// }
+
+// It is possible when the functions-body statement is in one row the following syntax
+// let add = (a, b) => a + b;
+
+function calculate(operation, firstOperand, secondOperand) {
+	let result = operation(firstOperand, secondOperand);
+	return result;
+}
+
+let sum = calculate(add, 5, 10);
+
+console.log('DEBUG sum: ' + sum)
