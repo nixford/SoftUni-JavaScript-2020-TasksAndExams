27b3e6b22c18d637a610d1221e4c7d269b28b807
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-04-07 20:25:45 +0300

    Add pure/impure functions

--- JS-Advanced/Tasks/Advanced-Functions-Lab/Lab-Coding.js
+++ JS-Advanced/Tasks/Advanced-Functions-Lab/Lab-Coding.js
@@ -1,54 +1,54 @@
 // FIRST CLASS / HIGHER-ORDER FUNCTIONS
 
-// function add(a, b) { 
-// 	return a + b;
-// }
+function add(a, b) { 
+	return a + b;
+}
 
-// // It is same as:
-// let add = function(a, b) { 
-// 	return a + b;
-// }
-// // It is same as:
-// let add = (a, b) => { 
-// 	return a + b;
-// }
+// It is same as:
+let add = function(a, b) { 
+	return a + b;
+}
+// It is same as:
+let add = (a, b) => { 
+	return a + b;
+}
 
-// // It is possible when the functions-body statement is in one row the following syntax
-// let add = (a, b) => a + b;
-// let multiply = (a, b) => a * b;
+// It is possible when the functions-body statement is in one row the following syntax
+let add = (a, b) => a + b;
+let multiply = (a, b) => a * b;
 
-// function calculate(operation, firstOperand, secondOperand) {
-// 	let result = operation(firstOperand, secondOperand);
-// 	return result;
-// }
+function calculate(operation, firstOperand, secondOperand) {
+	let result = operation(firstOperand, secondOperand);
+	return result;
+}
 
-// let sum = calculate(add, 5, 10);
-// let product = calculate(multiply, 5, 10);
+let sum = calculate(add, 5, 10);
+let product = calculate(multiply, 5, 10);
 
-// console.log(sum)
-// console.log(product)
-// console.log(multiply)
+console.log(sum)
+console.log(product)
+console.log(multiply)
 
 
 // Return function
-// function getOperation() {
-// 	return function(a, b) {
-// 		return a + b;
-// 	}
-// }
+function getOperation() {
+	return function(a, b) {
+		return a + b;
+	}
+}
 
 // Same as
-// function getOperation() {
-// 	let add = function(a, b) {
-// 		return a + b;
-// 	}
-// 	return add;
-// }
+function getOperation() {
+	let add = function(a, b) {
+		return a + b;
+	}
+	return add;
+}
 
-// // Operation is a function and it can be executed with providing numbers
-// let operation = getOperation();
+// Operation is a function and it can be executed with providing numbers
+let operation = getOperation();
 
-// console.log(operation(10, 20))
+console.log(operation(10, 20))
 
 
 // PREDICATES
@@ -60,10 +60,41 @@ let isValidName = function(name) {
 }
 
 // Same as
-let isValidName = function(name) {
+let isValidName = name => {
 	return name.split(' ').length == 2;
 }
 
-console.log(isValidName('Ivan Ivanov'));
-console.log(isValidName('Ivan Ivanov Petrov'));
-console.log(isValidName('Pesho'));
+// Same as
+let isValidName = name => name.split(' ').length == 2;
+
+// console.log(isValidName('Ivan Ivanov'));
+// console.log(isValidName('Ivan Ivanov Petrov'));
+// console.log(isValidName('Pesho'));
+
+let names = [
+	'Pesho',
+	'Ivan Petrov',
+	'Gosho Ivanov Petrov',
+	'Mariyka Petrova'
+];
+
+// Can be used, as follows
+let filteredNames = names.filter(isValidName);
+Also
+let filteredNamesTwo = names.filter(name => name.split(' ').length == 2);
+Also
+let filteredNamesThree = names.filter(x => isValidName(x)); // not necessery extention of the code
+
+console.log(filteredNames);
+console.log(filteredNamesTwo);
+console.log(filteredNamesThree);
+
+
+// PURE FUNCTIONS - returns always the same result by given input - doesn't depend on system state
+let add = (a, b) => a + b;
+
+// IMPURE FUNCTIONS - depend on system state
+let impureFunction = function(text) {
+	return new Date() + text;
+}
+console.log(impureFunction('Pesho'));
