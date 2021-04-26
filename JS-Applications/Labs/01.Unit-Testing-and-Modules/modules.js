87e1871405a968700b1481e35f22b98de4070f64
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-04-26 18:44:44 +0300

    Add IIFE module

--- JS-Applications/Labs/01.Unit-Testing-and-Modules/modules.js
+++ JS-Applications/Labs/01.Unit-Testing-and-Modules/modules.js
@@ -1,4 +1,17 @@
-function printMessage () {
-    console.log('Hello from console - function');
-}
+// message can be accessed globally - it can be limited with IIFE
+// let message = 'Hello from console - function';
+
+// function printMessage(additionalMessage) {
+//     console.log(message, additionalMessage);
+// }
+
+(function(scope) {
+    let message = 'Hello from console - function';
+
+    function printMessage(additionalMessage) {
+        console.log(message, additionalMessage);
+    }
+
+    scope.printMessage = printMessage;
+})(window);
 
