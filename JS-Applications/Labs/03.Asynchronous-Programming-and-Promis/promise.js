809ba5bba604ca38a832a82c07376162aa3c6954
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-05-03 21:26:38 +0300

    Add fetch requests and chaining promises

--- JS-Applications/Labs/03.Asynchronous-Programming-and-Promis/promise.js
+++ JS-Applications/Labs/03.Asynchronous-Programming-and-Promis/promise.js
@@ -1,12 +1,27 @@
-let promise = new Promise((resolve, reject) => {
-    console.log('Prepare start!!!');
+// function letsStart() {
+//     let promise = new Promise((resolve, reject) => {
+//         console.log('Prepare start!!!');
+    
+//         setTimeout(function() {
+//             resolve('Start!!!'); // Always resolves with start
+            // reject('Cancelled!!!'); // Always resolves with start
+//         }, 1000);    
+//     });
 
-    setTimeout(function() {
-        // resolve('Start!!!'); // Always resolves with start
-        // reject('Cancelled!!!'); // Always resolves with start
-    }, 1000);    
-});
+//     return promise;
+// }
 
+// Same as
+function letsStart() {
+	console.log('Prepare start!!!');
+    return Promise.resolve('Start!!!');
+}
+
+
+
+let promise = letsStart();
+
+// console.log(promise); // Pending promise
 promise
     .then(function(res) { // in case of resolve
         console.log('Success!!!');
@@ -19,3 +34,6 @@ promise
     .finally(() => { // executes always case of reject
         console.log('The end');
     });
+
+// console.log(promise); // Again will be pending promise
+// console.log('some rondom text here'); // will be executed before promise, because the primise execution is async
