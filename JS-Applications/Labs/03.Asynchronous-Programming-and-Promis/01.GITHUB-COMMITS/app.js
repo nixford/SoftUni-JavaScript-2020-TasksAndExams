95f772e44f7fb985c2d0d8680859ad584eb9dca8
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-05-04 17:36:31 +0300

    Add Github commits solution

--- JS-Applications/Labs/03.Asynchronous-Programming-and-Promis/01.GITHUB-COMMITS/app.js
+++ JS-Applications/Labs/03.Asynchronous-Programming-and-Promis/01.GITHUB-COMMITS/app.js
@@ -1,4 +1,16 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let username = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+    let commitsElement = document.getElementById('commits');
+    let url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            let commits = data.map(c => `<li>${c.commit.author.name}: ${c.commit.message}</li>`).join('');
+            commitsElement.innerHTML = commits;
+        })
+        .catch(err => {
+            commitsElement.innerHTML = `<li>Error: ${err.status} (${err.statusText})</li>`;
+        });
 }
