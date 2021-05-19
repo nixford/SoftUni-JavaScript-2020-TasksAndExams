67d8efd088b85810ee17e8b20b5829b991a971b9
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-05-19 19:09:02 +0300

    Add login route

--- JS-Applications/Course-Projects/01.Movies/Movies-Project/script.js
+++ JS-Applications/Course-Projects/01.Movies/Movies-Project/script.js
@@ -1,17 +1,30 @@
-function addEventListener() {
-    document.querySelector('.navigation').addEventListener('click', e => {
-        e.preventDefault();
-
-        if (!e.target.classList.contains('nav-link')) {
-            return;
-        }
-
-        let url = new URL(e.target.href);
-
-        console.log(url);
-
-        history.pushState({}, '', url.pathname);
-    });
+const routes = {
+    'login': 'login-form-template'
 }
 
-addEventListener();
+const router = (path) => {
+    let app = document.getElementById('app');
+    // Handlebars.compile creates function which return HTML
+    let template = Handlebars.compile(document.getElementById(routes[path]).innerHTML);
+    app.innerHTML = template();
+};
+
+function addEventListeners() {
+    document.querySelector('.navigation').addEventListener('click', navigateHandler);
+}
+
+function navigateHandler(e) {
+    e.preventDefault();
+
+    if (!e.target.classList.contains('nav-link')) {
+        return;
+    }
+
+    let url = new URL(e.target.href);
+
+    history.pushState({}, '', url.pathname);
+
+    router(url.pathname.slice(1));
+}
+
+addEventListeners();
