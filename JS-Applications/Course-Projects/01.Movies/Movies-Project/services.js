91b09c9c589b532e4663576ce94f46a68b46b531
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-05-24 19:11:20 +0300

    Make authService async function

--- JS-Applications/Course-Projects/01.Movies/Movies-Project/services.js
+++ JS-Applications/Course-Projects/01.Movies/Movies-Project/services.js
@@ -1,8 +1,9 @@
 const apiKey = 'AIzaSyDuIoJstS-cz6VnpUD6PT0-Q6uhxij9MSc';
 
+// ASYNC FUNCTION
 const authService = {
-  login(email, password) {
-    fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+  async login(email, password) {
+    let respose = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
@@ -11,13 +12,37 @@ const authService = {
         email,
         password,
       })
-    })
-    .then(res => res.json())
-    .then(data => {
-      // We have to save idToken in order to have auhorized requests to the server (in order not to login every time)
-      // therefore we save them locally with localStorage
-      localStorage.setItem('auth', JSON.stringify(data)); 
-      // In localStorage we cannot save objects - thr must be serializt with JSON.stringify(data)
-    })
+    });
+
+    let data = await respose.json;
+    // We have to save idToken in order to have auhorized requests to the server (in order not to login every time)
+    // therefore we save them locally with localStorage
+    localStorage.setItem('auth', JSON.stringify(data)); 
+    // In localStorage we cannot save objects - thr must be serializt with JSON.stringify(data)
+
+    return data;
   }
 }
+
+// NOT ASYNC FUNCTION
+// const authService = {
+//   login(email, password) {
+//     fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+//       method: 'POST',
+//       headers: {
+//         'content-type': 'application/json',
+//       },
+//       body: JSON.stringify({ // wrap/serialize the object with JSON.stringify
+//         email,
+//         password,
+//       })
+//     })
+//     .then(res => res.json())
+//     .then(data => {
+//       // We have to save idToken in order to have auhorized requests to the server (in order not to login every time)
+//       // therefore we save them locally with localStorage
+//       localStorage.setItem('auth', JSON.stringify(data)); 
+//       // In localStorage we cannot save objects - thr must be serializt with JSON.stringify(data)
+//     })
+//   }
+// }
