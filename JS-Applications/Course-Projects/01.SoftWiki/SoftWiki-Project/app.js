1e4c100ba134f93384d708e636f87493182f2bd6
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-06-13 18:43:11 +0300

    Add login.js and router

--- JS-Applications/Course-Projects/01.SoftWiki/SoftWiki-Project/app.js
+++ JS-Applications/Course-Projects/01.SoftWiki/SoftWiki-Project/app.js
@@ -1,5 +1,29 @@
 import { html, render } from './node_modules/lit-html/lit-html.js';
 import layout from './views/layout.js';
 import home from '/views/home.js';
+import login from './views/login.js';
 
-render(layout(home()), document.getElementById('app'));
+const routes = [
+  {
+    path: '/',
+    template: home,
+  },
+  {
+    path: '/login',
+    template: login,
+  },
+];
+
+const router = (path) => {
+  let route = routes.find(x => x.path == path);  
+
+  render(layout(route.template()), document.getElementById('app'));
+};
+
+function navigationHandler(e) {
+  if (e.target.tagName == 'A') {
+    
+  }
+}
+
+router(location.pathname);
--- JS-Applications/Course-Projects/01.SoftWiki/SoftWiki-Project/views/header.js
+++ JS-Applications/Course-Projects/01.SoftWiki/SoftWiki-Project/views/header.js
@@ -2,13 +2,14 @@ import { html } from '../node_modules/lit-html/lit-html.js';
 // import { html, render } from 'https://unpkg.com/lit-html?module';
 
 export default () => html`
-    <!-- Header -->
-    <header>
-      <h1><a class="home" href="#/home">SoftWiki</a></h1>
-      <nav class="nav-buttons">
-        <a href="#/create">Create</a>
-        <a href="#/logout">Logout</a>
-        <a href="#/register">Register</a>
-      </nav>
-    </header>
+<!-- Header -->
+<header>
+  <h1><a class="home" href="/">SoftWiki</a></h1>
+  <nav class="nav-buttons">
+    <a href="/login">Login</a>
+    <a href="/create">Create</a>
+    <a href="/logout">Logout</a>
+    <a href="/register">Register</a>
+  </nav>
+</header>
 `;
