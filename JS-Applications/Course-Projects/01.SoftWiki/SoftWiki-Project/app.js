a44a112ff2eab90541708078b5da4d1246279154
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-06-13 18:21:02 +0300

    Add home template

--- JS-Applications/Course-Projects/01.SoftWiki/SoftWiki-Project/app.js
+++ JS-Applications/Course-Projects/01.SoftWiki/SoftWiki-Project/app.js
@@ -1,4 +1,5 @@
 import { html, render } from './node_modules/lit-html/lit-html.js';
 import layout from './views/layout.js';
+import home from '/views/home.js';
 
-render(layout(), document.getElementById('app'));
+render(layout(home()), document.getElementById('app'));
--- JS-Applications/Course-Projects/01.SoftWiki/SoftWiki-Project/views/layout.js
+++ JS-Applications/Course-Projects/01.SoftWiki/SoftWiki-Project/views/layout.js
@@ -3,10 +3,8 @@ import { html } from '../node_modules/lit-html/lit-html.js';
 import header from './header.js'; // .js because there is no webpack
 import footer from './footer.js';
 
-export default () => html`
+export default (children) => html`
     ${header()}
-    <div>
-        <h2>something between</h2>
-    </div>
+    ${children}
     ${footer()}
 `;
